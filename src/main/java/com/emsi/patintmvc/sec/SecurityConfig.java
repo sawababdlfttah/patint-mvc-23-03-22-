76233822b83bc9d8de0f21d444937305d03c005a
abdelfattah sawab <sawab_abdelfattah@emsi-edu.ma>
2022-04-19 12:00:18 +0000

    COMMAIN

--- src/main/java/com/emsi/patintmvc/sec/SecurityConfig.java
+++ src/main/java/com/emsi/patintmvc/sec/SecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
-/////
+////////////////
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
