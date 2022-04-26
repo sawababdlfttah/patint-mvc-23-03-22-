6456da4905be82debc3ccc7f5b42a2479e322a07
abdelfattah sawab <SAWAB_ABDELFATTAH@emsi-edu.ma>
2022-04-26 10:24:21 +0000

    PARTE3 FINAL

--- src/main/java/com/emsi/patintmvc/PatintMvcApplication.java
+++ src/main/java/com/emsi/patintmvc/PatintMvcApplication.java
@@ -5,7 +5,6 @@ import com.emsi.patintmvc.repositories.PatientRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 import java.util.Date;
 
